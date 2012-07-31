710a98299ffc867fa3a53abcd5e642f8a72d5cb6
rqshao <rqshao@445347b9-a622-0248-9c6a-1ca700c04f83>
2012-07-31 09:09:49 +0000

    db constant
    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@100 445347b9-a622-0248-9c6a-1ca700c04f83

---
+++ ibilling/grails-app/controllers/ibilling/DBConstantController.groovy
@@ -0,0 +1,115 @@
+
+package ibilling
+
+import grails.plugins.springsecurity.Secured
+import com.infosense.ibilling.server.user.contact.db.ContactTypeDTO
+import com.infosense.ibilling.server.user.contact.db.ContactDTO
+import com.infosense.ibilling.server.user.db.CompanyDTO
+import com.infosense.ibilling.server.util.db.LanguageDTO
+import com.infosense.ibilling.server.util.db.InternationalDescription
+import com.infosense.ibilling.server.ws.ContactTypeWS;
+import com.infosense.ibilling.server.ws.InternationalDescriptionWS;
+import com.infosense.ibilling.common.SessionInternalError
+
+import com.infosense.ibilling.server.util.db.IbillingConstant
+import com.infosense.ibilling.server.util.db.IbillingConstantDAS
+@Secured(["MENU_99"])
+class DBConstantController {
+
+    def viewUtils
+	def messageSource
+    def recentItemService
+    def breadcrumbService
+	def ibillingConstantDAS
+
+    def index = {
+        redirect action: list, params: params
+    }
+
+    def list = {
+
+        def constants = ibillingConstantDAS.findAll()
+
+        def selected = params.id ? ibillingConstantDAS.find(params.int("id")) : null
+
+        breadcrumbService.addBreadcrumb(controllerName, actionName, null, null)
+
+        [ constants: constants, selected: selected]
+    }
+	
+    def show = {
+        def selected = ibillingConstantDAS.find(params.int('id'))
+        breadcrumbService.addBreadcrumb(controllerName, 'list', null, params.int('id'))
+
+        render template: 'show', model: [ selected: selected]
+    }
+
+    def add = {
+        render template: 'show'
+    }
+
+    def save = {
+        def ibillingConstant = new IbillingConstant()
+		Locale locale = session.locale
+		def tid = params?.id?:''
+		Integer cid = 0
+		if(tid != ''){
+				cid = tid as Integer
+		}
+		def name = params?.name?:''
+		def content = params?.content?:''
+		def create = false
+		if(name == '' || content == ''){
+			flash.error = messageSource.getMessage("dbconstant.name.required",null,locale);
+			redirect action: list
+			return
+		}
+		if(cid == 0 ){
+			def table = ibillingConstantDAS.findByName(name)
+			if(table !=null){
+				flash.error = messageSource.getMessage("dbconstant.name.unique",[name].toArray(), locale)
+				redirect action: list
+				return
+			}
+			create = true
+		}
+		
+		bindData(ibillingConstant, params)
+		
+        try {
+            log.debug("creating new constant")
+            ibillingConstant = ibillingConstantDAS.save(ibillingConstant)
+			cid = ibillingConstant.getId()
+			if(create){
+				flash.message = 'dbconstant.name.create'
+			}else{
+			flash.message = 'dbconstant.name.update'
+			}
+			
+
+        } catch (SessionInternalError e) {
+            viewUtils.resolveException(flash, session.locale, e)
+        }
+
+        redirect action:list, id:cid
+    }
+	
+	def delete = {
+		
+		try {
+			Integer id = params.id as Integer;
+			IbillingConstant ibillingConstant = ibillingConstantDAS.find(id)
+			if(ibillingConstant != null && ibillingConstant.getId() >0 ){
+				ibillingConstantDAS.delete(ibillingConstant)
+				flash.message = messageSource.getMessage("dbconstant.delete.done",[ibillingConstant.getName()].toArray(), session.locale);
+			}else{
+			flash.error = messageSource.getMessage("dbconstant.delete.none",[ibillingConstant.getName()].toArray(), session.locale);
+			}
+			
+		} catch (SessionInternalError e) {
+			viewUtils.resolveException(flash, session.locale, e);
+		}
+		redirect (action:list)
+	}
+}
+

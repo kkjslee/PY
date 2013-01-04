010dc08a71137a14b5f4748b0ffa7756a2aab97c
jpshao <jpshao@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-04 09:31:10 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@561 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/resources/import.sql
+++ Openstack/src/main/resources/import.sql
@@ -1,3 +1,3 @@
 -- You can use this file to load seed data into the database using SQL statements
 Insert into Language(id, name, language, country) values (1, 'English', 'en', 'US')
-Insert into Language(id, name, language, country) values (2, 'ÖÐÎÄ', 'zh', 'CN')
+Insert into Language(id, name, language, country) values (2, 'ä¸­æ–‡', 'zh', 'CN')

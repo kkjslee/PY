fd2be3b178173eeb2b5758d4f71d54771f5f75a0
lwan <lwan@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-05 06:22:33 +0000

    Apply token
    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@569 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/test/resources/import.sql
+++ Openstack/src/test/resources/import.sql
@@ -1,2 +1,8 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+insert into Language(id, name, language, country) values (1, 'English', 'en', 'US')
+insert into Language(id, name, language, country) values (2, '中文', 'zh', 'CN')
+
+insert into Configuration(name, value) values('openstack.endpoint.token', 'http://192.168.1.145:5000/v2.0/tokens')
+insert into Configuration(name, value) values('openstack.tenant.admin', '8384b45a9ad34a5da3a0a2f8b12b99bb')
+insert into Configuration(name, value) values('openstack.user.admin.name', 'admin')
+insert into Configuration(name, value) values('openstack.user.admin.pass', 'inforstack')

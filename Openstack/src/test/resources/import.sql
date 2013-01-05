a904488382f0e5d01a037081e0b64c16e86af375
lwan <lwan@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-05 08:25:10 +0000

    Query Host RestTemplate utilities
    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@572 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/test/resources/import.sql
+++ Openstack/src/test/resources/import.sql
@@ -3,6 +3,7 @@ insert into Language(id, name, language, country) values (1, 'English', 'en', 'U
 insert into Language(id, name, language, country) values (2, '中文', 'zh', 'CN')
 
 insert into Configuration(name, value) values('openstack.endpoint.token', 'http://192.168.1.145:5000/v2.0/tokens')
+insert into Configuration(name, value) values('openstack.endpoint.hosts', 'http://192.168.1.145:8774/v2/{tenant}/os-hosts')
 insert into Configuration(name, value) values('openstack.tenant.admin', '8384b45a9ad34a5da3a0a2f8b12b99bb')
 insert into Configuration(name, value) values('openstack.user.admin.name', 'admin')
 insert into Configuration(name, value) values('openstack.user.admin.pass', 'inforstack')

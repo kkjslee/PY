92221f8885f040ca6e9046ff6aa96d518319bd8f
lwan <lwan@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-06 03:50:47 +0000

    Query host and host describe
    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@580 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/resources/import.sql
+++ Openstack/src/main/resources/import.sql
@@ -3,6 +3,8 @@ Insert into Language(id, name, language, country) values (1, 'English', 'en', 'U
 Insert into Language(id, name, language, country) values (2, '中文', 'zh', 'CN')
 
 insert into Configuration(name, value) values('openstack.endpoint.token', 'http://192.168.1.145:5000/v2.0/tokens')
+insert into Configuration(name, value) values('openstack.endpoint.tenant', 'http://192.168.1.145:5000/v2.0/tenants')
+insert into Configuration(name, value) values('openstack.endpoint.user', 'http://192.168.1.145:5000/v2.0/users')
 insert into Configuration(name, value) values('openstack.endpoint.hosts', 'http://192.168.1.145:8774/v2/{tenant}/os-hosts')
 insert into Configuration(name, value) values('openstack.endpoint.host.describe', 'http://192.168.1.145:8774/v2/{tenant}/os-hosts/{host}')
 insert into Configuration(name, value) values('openstack.tenant.admin', '8384b45a9ad34a5da3a0a2f8b12b99bb')

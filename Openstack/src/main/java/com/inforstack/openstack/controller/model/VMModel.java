bfba989bbb4089171de9c57d40894f6aa0b5cab5
rqshao <rqshao@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-09 04:02:50 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@665 445347b9-a622-0248-9c6a-1ca700c04f83

---
+++ Openstack/src/main/java/com/inforstack/openstack/controller/model/VMModel.java
@@ -0,0 +1,30 @@
+package com.inforstack.openstack.controller.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * vm instance model
+ * 
+ * @author shaw
+ * 
+ */
+public class VMModel {
+  //vm uuid
+  private String vmid;
+  private String vmname;
+  //cpu number
+  private Integer cpus;
+  //memory size
+  private Integer memory;
+  //vm create time
+  private Date starttime;
+  //vm expire time
+  private Date expiretime;
+  //public up bound
+  private List<String> publcips;
+  private String privateip;
+  private Integer disksize;
+  private Integer maxmemory;
+  private 
+}

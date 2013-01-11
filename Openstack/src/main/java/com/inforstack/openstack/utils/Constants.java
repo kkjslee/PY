7fa1f0a01283f33f9c848879d53e00b60ac8140b
rqshao <rqshao@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-11 01:56:43 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@686 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/java/com/inforstack/openstack/utils/Constants.java
+++ Openstack/src/main/java/com/inforstack/openstack/utils/Constants.java
@@ -1,16 +1,43 @@
 package com.inforstack.openstack.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Constants {
-	
-	public static final String SESSION_ATTRIBUTE_NAME_AGENT = Constants.class.getName() + ".Agent";
-	
-	public static final int USER_STATUS_INVALID = 0;
-	public static final int USER_STATUS_VALID = 1;
-	
-	public static final int USER_AGEING_ACTIVE = 1;
-	public static final int USER_AGEING_DELETED = 2;
-	
-	public static final int TENANT_AGEING_ACTIVE = 1;
-	public static final int TENANT_AGEING_DELETED = 2;
-	
+
+  public static final String SESSION_ATTRIBUTE_NAME_AGENT = Constants.class.getName() + ".Agent";
+
+  public static final int USER_STATUS_INVALID = 0;
+  public static final int USER_STATUS_VALID = 1;
+
+  public static final int USER_AGEING_ACTIVE = 1;
+  public static final int USER_AGEING_DELETED = 2;
+
+  public static final int TENANT_AGEING_ACTIVE = 1;
+  public static final int TENANT_AGEING_DELETED = 2;
+
+  public static final int IMG_PROGRESS_ACTIVE = 100;
+  public static final String IMG_STATUS_ACTIVE = "ACTIVE";
+
+  public static final String IMG_METADATA_KEY_KERNEL = "kernel_id";
+  public static final String IMG_METADATA_KEY_RAMDISK = "ramdisk_id";
+
+  public static final String JSON_STATUS_EXCEPTION = "{\"status\":\"exception\"}";
+  public static final String JSON_STATUS_DONE = "{\"status\":\"done\"}";
+  public static final String JSON_STATUS_FAILED = "{\"status\":\"failed\"}";
+
+  // compatible with font side
+  public static final Map<String, String> vmStatusMap = new HashMap<String, String>();
+  // key is the same as openstack status, and value is the same as front
+  static {
+    vmStatusMap.put("active", "RUNNING");
+    vmStatusMap.put("pause", "PAUSED");
+    vmStatusMap.put("rebooting", "RESUMING");
+    vmStatusMap.put("building", "CREATING");
+    vmStatusMap.put("stopped", "SHUTOFF");
+    vmStatusMap.put("resized", "RESIZED");
+    vmStatusMap.put("suspended", "SUSPENDED");
+    vmStatusMap.put("resuming", "RESUMING");
+  }
+
 }

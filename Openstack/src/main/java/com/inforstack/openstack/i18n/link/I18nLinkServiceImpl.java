a900afc7fa928479f93714d60f331af3f406e37d
lwan <lwan@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-16 05:42:12 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@740 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/java/com/inforstack/openstack/i18n/link/I18nLinkServiceImpl.java
+++ Openstack/src/main/java/com/inforstack/openstack/i18n/link/I18nLinkServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.inforstack.openstack.i18n.I18nDao;
 import com.inforstack.openstack.utils.OpenstackUtil;
 import com.inforstack.openstack.utils.StringUtil;
 

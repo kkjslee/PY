0f85e60460e8102f893d739523ad3df31aaa7b7f
rqshao <rqshao@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-22 08:44:47 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@783 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/webapp/resources/common/js/common.js
+++ Openstack/src/main/webapp/resources/common/js/common.js
@@ -1,7 +1,16 @@
 function isNull(value) {
-    if (typeof(value) == "undefined" ||null == value|| "" == $.trim(value) || value == "n/a" || value.length == 0) {
+    if (typeof(value) == "undefined" ||null == value|| "" == $.trim(value) || value == "null" || value.length == 0) {
         return true;
     } else {
         return false;
     }
+}
+function formatDate(time){
+	if(isNull(time)){
+		return "";
+	}else {
+		var date = new Date();
+		date.setTime(time);
+		return date.toLocaleString();
+	}
 }

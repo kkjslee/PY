ae93a9c5c83fcb0e19937f430fe8d047d9750e34
lwan <lwan@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-01-30 03:33:55 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@819 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/test/resources/import.sql
+++ Openstack/src/test/resources/import.sql
@@ -33,11 +33,24 @@ insert into Role(id, name) values (1, 'admin')
 insert into Role(id, name) values (2, 'user')
 insert into Role(id, name) values (3, 'agent')
 
-insert into Tenant(id, name, dipalyName, role_id, ageing) values (1, 'admin', 'admin', 1, 1)
+insert into Tenant(id, name, display_name, role_id, ageing) values (1, 'admin', 'admin', 1, 1)
 
-insert into User(id, name, password, firstName, lastName, role_id, defaultTenantId, status, ageing) values (1, 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', 'admin', 'admin', 1, 1, 1, 1)
+insert into User(id, name, password, firstName, lastName, role_id, default_tenant_id, status, ageing) values (1, 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', 'admin', 'admin', 1, 1, 1, 1)
 
 insert into tenant_user(user_id, tenant_id) values (1, 1)
+
+insert into Dictionary(id, dict_key, code, language_id, value) values (1, 'period.type', '1', 1, 'Year')
+insert into Dictionary(id, dict_key, code, language_id, value) values (2, 'period.type', '1', 2, '年')
+insert into Dictionary(id, dict_key, code, language_id, value) values (3, 'period.type', '2', 1, 'Month')
+insert into Dictionary(id, dict_key, code, language_id, value) values (4, 'period.type', '2', 2, '月')
+insert into Dictionary(id, dict_key, code, language_id, value) values (5, 'period.type', '4', 1, 'Week')
+insert into Dictionary(id, dict_key, code, language_id, value) values (6, 'period.type', '4', 2, '周')
+insert into Dictionary(id, dict_key, code, language_id, value) values (7, 'period.type', '5', 1, 'Day')
+insert into Dictionary(id, dict_key, code, language_id, value) values (8, 'period.type', '5', 2, '天')
+insert into Dictionary(id, dict_key, code, language_id, value) values (9, 'period.type', '11', 1, 'Hour')
+insert into Dictionary(id, dict_key, code, language_id, value) values (10, 'period.type', '11', 2, '小时')
+insert into Dictionary(id, dict_key, code, language_id, value) values (11, 'period.hour', '12', 1, 'Minute')
+insert into Dictionary(id, dict_key, code, language_id, value) values (12, 'period.hour', '12', 2, '分钟')
 
 insert into Resource(id, url, permission) values (1, '/admin', 'admin')
 
@@ -50,7 +63,7 @@ insert into Resource(id, url, permission) values (7, '/admin/instance/createInst
 insert into Resource(id, url, permission) values (8, '/admin/instance/getInstance', 'admin');
 
 insert into Resource(id, url, permission) values (9, '/admin/image/getPagerImageList', 'admin');
-insert into Resource(id, url, permission) values (10, '/admin/image/imglist', 'admin');
+insert into Resource(id, url, permission) values (10, '/admin/image/imgList', 'admin');
 insert into Resource(id, url, permission) values (11, '/admin/image/createImage', 'admin');
 insert into Resource(id, url, permission) values (12, '/admin/image/retrieveImage', 'admin');
 
@@ -67,6 +80,16 @@ insert into Resource(id, url, permission) values (21, '/admin/scripts/bootstrap'
 insert into Resource(id, url, permission) values (22, '/admin/scripts/template', 'admin');
 insert into Resource(id, url, permission) values (23, '/admin/modules/entry/index', 'admin');
 
-
-insert into Resource(id, url, permission) values (24, '/admin/image/imgList', 'admin');
+insert into Resource(id, url, permission) values (24, '/admin/image/modules/index', 'admin');
 insert into Resource(id, url, permission) values (25, '/admin/flavor/flavorList', 'admin');
+insert into Resource(id, url, permission) values (26, '/admin/image/scripts/bootstrap', 'admin');
+insert into Resource(id, url, permission) values (27, '/admin/image/scripts/template', 'admin');
+insert into Resource(id, url, permission) values (28, '/admin/image/getPagerAllImageList', 'admin');
+insert into Resource(id, url, permission) values (29, '/admin/flavor/getPagerAllFlavorList', 'admin');
+insert into Resource(id, url, permission) values (30, '/admin/flavor/nameCheck', 'admin');
+
+insert into Resource(id, url, permission) values (31, '/user', 'user')
+insert into Resource(id, url, permission) values (32, '/user/scripts/navinit', 'user');
+insert into Resource(id, url, permission) values (33, '/user/scripts/bootstrap', 'user');
+insert into Resource(id, url, permission) values (34, '/user/scripts/template', 'user');
+insert into Resource(id, url, permission) values (35, '/user/modules/entry/index', 'user');

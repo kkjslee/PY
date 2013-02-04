1a758907b54dda44741ac1dd867f966dc883e1e0
rqshao <rqshao@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-02-04 03:09:28 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@848 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/java/com/inforstack/openstack/controller/admin/ProductController.java
+++ Openstack/src/main/java/com/inforstack/openstack/controller/admin/ProductController.java
@@ -1,6 +1,9 @@
 package com.inforstack.openstack.controller.admin;
 
+import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,17 +11,27 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.inforstack.openstack.controller.model.CategoryModel;
+import com.inforstack.openstack.controller.model.I18nModel;
 import com.inforstack.openstack.controller.model.ItemSpecificationModel;
+import com.inforstack.openstack.controller.model.PagerModel;
 import com.inforstack.openstack.controller.model.PriceModel;
+import com.inforstack.openstack.controller.model.ProfileModel;
 import com.inforstack.openstack.exception.ApplicationException;
+import com.inforstack.openstack.i18n.lang.Language;
+import com.inforstack.openstack.i18n.lang.LanguageService;
+import com.inforstack.openstack.item.Category;
 import com.inforstack.openstack.item.ItemService;
 import com.inforstack.openstack.item.ItemSpecification;
+import com.inforstack.openstack.item.Profile;
 import com.inforstack.openstack.log.Logger;
 import com.inforstack.openstack.utils.Constants;
+import com.inforstack.openstack.utils.OpenstackUtil;
 
 @Controller
 @RequestMapping(value = "/admin/product")
@@ -27,6 +40,9 @@ public class ProductController {
 
   @Autowired
   private ItemService itemService;
+
+  @Autowired
+  private LanguageService languageService;
 
   private final String ADMIN_PRODUCT_MODULE_HOME = "admin/modules/Product";
 
@@ -39,18 +55,108 @@ public class ProductController {
   public String bootstrapForAdminCategory(Model model) {
     model.addAttribute(Constants.PAGER_PAGE_INDEX, Constants.DEFAULT_PAGE_INDEX);
     model.addAttribute(Constants.PAGER_PAGE_SIZE, Constants.DEFAULT_PAGE_SIZE);
+    List<Language> lList = new ArrayList<Language>();
+    lList = languageService.list();
+    model.addAttribute("lList", lList);
     return ADMIN_PRODUCT_MODULE_HOME + "/scripts/bootstrap";
   }
 
   @RequestMapping(value = "/scripts/template", method = RequestMethod.GET)
   public String templateForAdminCategory(Model model) {
+    List<Language> lList = new ArrayList<Language>();
+    lList = languageService.list();
+    model.addAttribute("lList", lList);
     return ADMIN_PRODUCT_MODULE_HOME + "/scripts/template";
+  }
+
+  @RequestMapping(value = "/listForJsp", method = RequestMethod.POST)
+  public String listProducts(Model model, Integer pageIndex, Integer pageSize) {
+    int pageIdx = -1;
+    int pageSze = 0;
+    if (pageIndex == null || pageIndex == 0) {
+      log.info("no pageindex passed, set default value 1");
+      pageIdx = Constants.DEFAULT_PAGE_INDEX;
+    } else {
+      pageIdx = pageIndex;
+    }
+    if (pageSize == null) {
+      log.info("no page size passed, set default value 20");
+      pageSze = Constants.DEFAULT_PAGE_SIZE;
+    } else {
+      pageSze = pageSize;
+    }
+    Integer languageId = OpenstackUtil.getLanguage().getId();
+    List<Category> categories = this.itemService.listAllCategory(false);
+    List<ItemSpecificationModel> models = new ArrayList<ItemSpecificationModel>();
+    for (Category category : categories) {
+      I18nModel[] name = new I18nModel[1];
+      name[0] = new I18nModel();
+      name[0].setLanguageId(languageId);
+      name[0].setContent(category.getName().getI18nContent());
+
+      CategoryModel cm = new CategoryModel();
+      cm.setId(category.getId());
+      cm.setName(name);
+      cm.setEnable(category.getEnable());
+
+      List<ItemSpecification> itemSpecifications = this.itemService
+          .listItemSpecificationByCategory(category);
+
+      for (ItemSpecification itemSpecification : itemSpecifications) {
+        I18nModel[] itemName = new I18nModel[1];
+        itemName[0] = new I18nModel();
+        itemName[0].setLanguageId(languageId);
+        itemName[0].setContent(itemSpecification.getName().getI18nContent());
+
+        ItemSpecificationModel itemSpecificationModel = new ItemSpecificationModel();
+        itemSpecificationModel.setId(itemSpecification.getId());
+        itemSpecificationModel.setName(itemName);
+        itemSpecificationModel.setOsType(itemSpecification.getOsType());
+        itemSpecificationModel.setRefId(itemSpecification.getRefId());
+        itemSpecificationModel.setDefaultPrice(itemSpecification.getDefaultPrice());
+        itemSpecificationModel.setAvailable(itemSpecification.getAvailable());
+        itemSpecificationModel.setCreated(DateFormat.getDateTimeInstance(DateFormat.SHORT,
+            DateFormat.SHORT, OpenstackUtil.getLocale()).format(itemSpecification.getCreated()));
+        itemSpecificationModel.setUpdated(DateFormat.getDateTimeInstance(DateFormat.SHORT,
+            DateFormat.SHORT, OpenstackUtil.getLocale()).format(itemSpecification.getUpdated()));
+
+        Profile profile = itemSpecification.getProfile();
+        if (profile != null) {
+          ProfileModel profileModel = new ProfileModel();
+          profileModel.setItem(itemSpecification.getId());
+          if (profile.getCpu() != null) {
+            profileModel.setCpu(profile.getCpu().getId());
+          }
+          if (profile.getMemory() != null) {
+            profileModel.setMemory(profile.getMemory().getId());
+          }
+          if (profile.getDisk() != null) {
+            profileModel.setDisk(profile.getDisk().getId());
+          }
+          if (profile.getNetwork() != null) {
+            profileModel.setNetwork(profile.getNetwork().getId());
+          }
+
+          itemSpecificationModel.setProfile(profileModel);
+        }
+
+        models.add(itemSpecificationModel);
+      }
+    }
+    PagerModel<ItemSpecificationModel> page = new PagerModel<ItemSpecificationModel>(models,
+        pageSze);
+    models = page.getPagedData(pageIdx);
+    model.addAttribute("pageIndex", pageIdx);
+    model.addAttribute("pageSize", pageSze);
+    model.addAttribute("pageTotal", page.getTotalRecord());
+    model.addAttribute("dataList", models);
+    return ADMIN_PRODUCT_MODULE_HOME + "/tr";
   }
 
   @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
   public @ResponseBody
   Map<String, Object> createItemSpecification(Model model,
-      ItemSpecificationModel itemSpecificationModel) {
+      @RequestBody ItemSpecificationModel itemSpecificationModel) {
     Map<String, Object> returnValue = new HashMap<String, Object>();
     if (itemSpecificationModel != null) {
       try {
@@ -60,6 +166,7 @@ public class ProductController {
         }
       } catch (ApplicationException e) {
         log.error(e.getMessage(), e);
+        returnValue.put("error", e.getMessage());
       }
 
     }
@@ -68,7 +175,7 @@ public class ProductController {
 
   @RequestMapping(value = "/editPrice", method = RequestMethod.POST, produces = "application/json")
   public @ResponseBody
-  Map<String, Object> editPrice(Model model, PriceModel priceModel) {
+  Map<String, Object> editPrice(Model model, @RequestBody PriceModel priceModel) {
     Map<String, Object> returnValue = new HashMap<String, Object>();
     if (priceModel != null) {
       try {
@@ -76,6 +183,7 @@ public class ProductController {
         returnValue.put("success", "success");
       } catch (ApplicationException e) {
         log.error(e.getMessage(), e);
+        returnValue.put("error", e.getMessage());
       }
 
     }
--- Openstack/src/main/java/com/inforstack/openstack/controller/model/ItemSpecificationModel.java
+++ Openstack/src/main/java/com/inforstack/openstack/controller/model/ItemSpecificationModel.java
@@ -1,121 +1,134 @@
 package com.inforstack.openstack.controller.model;
 
-
+import com.inforstack.openstack.utils.OpenstackUtil;
 
 public class ItemSpecificationModel {
-	
-	private Integer id;
-	
-	private I18nModel[] name;
-	
-	private float defaultPrice;
-	
-	private Integer osType;
-	
-	private String refId;
-	
-	private Boolean available;
-	
-	private String created;
-	
-	private String updated;
-	
-	private CategoryModel[] categories;
-	
-	private ItemMetadataModel[] metadata;
-	
-	private ProfileModel profile;
-	
-	public ItemSpecificationModel() {
-		
-	}
 
-	public Integer getId() {
-		return id;
-	}
+  private Integer id;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+  private I18nModel[] name;
 
-	public I18nModel[] getName() {
-		return name;
-	}
+  private float defaultPrice;
 
-	public void setName(I18nModel[] name) {
-		this.name = name;
-	}
+  private Integer osType;
 
-	public float getDefaultPrice() {
-		return defaultPrice;
-	}
+  private String refId;
 
-	public void setDefaultPrice(float defaultPrice) {
-		this.defaultPrice = defaultPrice;
-	}
+  private Boolean available;
 
-	public Integer getOsType() {
-		return osType;
-	}
+  private String created;
 
-	public void setOsType(Integer osType) {
-		this.osType = osType;
-	}
+  private String updated;
 
-	public String getRefId() {
-		return refId;
-	}
+  private CategoryModel[] categories;
 
-	public void setRefId(String refId) {
-		this.refId = refId;
-	}
+  private ItemMetadataModel[] metadata;
 
-	public Boolean isAvailable() {
-		return available;
-	}
+  private ProfileModel profile;
 
-	public void setAvailable(Boolean available) {
-		this.available = available;
-	}
+  public ItemSpecificationModel() {
 
-	public String getCreated() {
-		return created;
-	}
+  }
 
-	public void setCreated(String created) {
-		this.created = created;
-	}
+  public Integer getId() {
+    return id;
+  }
 
-	public String getUpdated() {
-		return updated;
-	}
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-	public void setUpdated(String updated) {
-		this.updated = updated;
-	}
+  public I18nModel[] getName() {
+    return name;
+  }
 
-	public CategoryModel[] getCategories() {
-		return categories;
-	}
+  public void setName(I18nModel[] name) {
+    this.name = name;
+  }
 
-	public void setCategories(CategoryModel[] categories) {
-		this.categories = categories;
-	}
+  public float getDefaultPrice() {
+    return defaultPrice;
+  }
 
-	public ItemMetadataModel[] getMetadata() {
-		return metadata;
-	}
+  public void setDefaultPrice(float defaultPrice) {
+    this.defaultPrice = defaultPrice;
+  }
 
-	public void setMetadata(ItemMetadataModel[] metadata) {
-		this.metadata = metadata;
-	}
+  public Integer getOsType() {
+    return osType;
+  }
 
-	public ProfileModel getProfile() {
-		return profile;
-	}
+  public void setOsType(Integer osType) {
+    this.osType = osType;
+  }
 
-	public void setProfile(ProfileModel profile) {
-		this.profile = profile;
-	}
+  public String getRefId() {
+    return refId;
+  }
+
+  public void setRefId(String refId) {
+    this.refId = refId;
+  }
+
+  public Boolean isAvailable() {
+    return available;
+  }
+
+  public void setAvailable(Boolean available) {
+    this.available = available;
+  }
+
+  public Boolean getAvailable() {
+    return available;
+
+  }
+
+  public String getEnabledDesc() {
+    if (available == true) {
+      return OpenstackUtil.getMessage("product.status.enabled");
+    } else {
+      return OpenstackUtil.getMessage("product.status.disabled");
+    }
+  }
+
+  public String getCreated() {
+    return created;
+  }
+
+  public void setCreated(String created) {
+    this.created = created;
+  }
+
+  public String getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(String updated) {
+    this.updated = updated;
+  }
+
+  public CategoryModel[] getCategories() {
+    return categories;
+  }
+
+  public void setCategories(CategoryModel[] categories) {
+    this.categories = categories;
+  }
+
+  public ItemMetadataModel[] getMetadata() {
+    return metadata;
+  }
+
+  public void setMetadata(ItemMetadataModel[] metadata) {
+    this.metadata = metadata;
+  }
+
+  public ProfileModel getProfile() {
+    return profile;
+  }
+
+  public void setProfile(ProfileModel profile) {
+    this.profile = profile;
+  }
 
 }

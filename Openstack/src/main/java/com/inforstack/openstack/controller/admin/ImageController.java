cb7ed1d34d0d2c13159c1d1be209ef8cffd7539a
rqshao <rqshao@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-02-05 05:04:35 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@877 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/java/com/inforstack/openstack/controller/admin/ImageController.java
+++ Openstack/src/main/java/com/inforstack/openstack/controller/admin/ImageController.java
@@ -7,8 +7,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,176 +31,183 @@ import com.inforstack.openstack.utils.ValidateUtil;
 @RequestMapping(value = "/admin/image")
 public class ImageController {
 
-  private static final Logger log = new Logger(ImageController.class);
+	private static final Logger log = new Logger(ImageController.class);
 
-  @Autowired
-  private ImageService imageService;
+	@Autowired
+	private ImageService imageService;
 
-  @Autowired
-  private KeystoneService keystoneService;
+	@Autowired
+	private KeystoneService keystoneService;
 
-  @Autowired
-  private Validator validator;
+	@Autowired
+	private Validator validator;
 
-  private final String IMAGE_MODULE_HOME = "admin/modules/Image";
+	private final String IMAGE_MODULE_HOME = "admin/modules/Image";
 
-  @RequestMapping(value = "/modules/index", method = RequestMethod.GET)
-  public String redirectModule(Model model, HttpServletRequest request) {
-    return IMAGE_MODULE_HOME + "/index";
-  }
+	@RequestMapping(value = "/modules/index", method = RequestMethod.GET)
+	public String redirectModule(Model model, HttpServletRequest request) {
+		return IMAGE_MODULE_HOME + "/index";
+	}
 
-  @RequestMapping(value = "/scripts/bootstrap", method = RequestMethod.GET)
-  public String bootstrap(Model model) {
-    model.addAttribute(Constants.PAGER_PAGE_INDEX, Constants.DEFAULT_PAGE_INDEX);
-    model.addAttribute(Constants.PAGER_PAGE_SIZE, Constants.DEFAULT_PAGE_SIZE);
-    return IMAGE_MODULE_HOME + "/scripts/bootstrap";
-  }
+	@RequestMapping(value = "/scripts/bootstrap", method = RequestMethod.GET)
+	public String bootstrap(Model model) {
+		model.addAttribute(Constants.PAGER_PAGE_INDEX,
+				Constants.DEFAULT_PAGE_INDEX);
+		model.addAttribute(Constants.PAGER_PAGE_SIZE,
+				Constants.DEFAULT_PAGE_SIZE);
+		return IMAGE_MODULE_HOME + "/scripts/bootstrap";
+	}
 
-  @RequestMapping(value = "/scripts/template", method = RequestMethod.GET)
-  public String template(Model model) {
-    return IMAGE_MODULE_HOME + "/scripts/template";
-  }
+	@RequestMapping(value = "/scripts/template", method = RequestMethod.GET)
+	public String template(Model model) {
+		return IMAGE_MODULE_HOME + "/scripts/template";
+	}
 
-  @RequestMapping(value = "/getPagerImageList", method = RequestMethod.POST)
-  public String getPagerImages(Model model, Integer pageIndex, Integer pageSize) {
-    int pageIdx = -1;
-    int pageSze = 0;
-    if (pageIndex == null || pageIndex == 0) {
-      log.info("no pageindex passed, set default value 1");
-      pageIdx = Constants.DEFAULT_PAGE_INDEX;
-    } else {
-      pageIdx = pageIndex;
-    }
-    if (pageSize == null) {
-      log.info("no page size passed, set default value 20");
-      pageSze = Constants.DEFAULT_PAGE_SIZE;
-    } else {
-      pageSze = pageSize;
-    }
-    getImages(model, pageIdx, pageSze, false);
-    return IMAGE_MODULE_HOME + "/tr";
-  }
+	@RequestMapping(value = "/getPagerImageList", method = RequestMethod.POST)
+	public String getPagerImages(Model model, Integer pageIndex,
+			Integer pageSize) {
+		int pageIdx = -1;
+		int pageSze = 0;
+		if (pageIndex == null || pageIndex == 0) {
+			log.info("no pageindex passed, set default value 1");
+			pageIdx = Constants.DEFAULT_PAGE_INDEX;
+		} else {
+			pageIdx = pageIndex;
+		}
+		if (pageSize == null) {
+			log.info("no page size passed, set default value 20");
+			pageSze = Constants.DEFAULT_PAGE_SIZE;
+		} else {
+			pageSze = pageSize;
+		}
+		getImages(model, pageIdx, pageSze, false);
+		return IMAGE_MODULE_HOME + "/tr";
+	}
 
-  // this not valid image meta data
-  @RequestMapping(value = "/getPagerAllImageList", method = RequestMethod.POST)
-  public String getPagerAllImages(Model model, Integer pageIndex, Integer pageSize) {
-    int pageIdx = -1;
-    int pageSze = 0;
-    if (pageIndex == null || pageIndex == 0) {
-      log.info("no pageindex passed, set default value 1");
-      pageIdx = Constants.DEFAULT_PAGE_INDEX;
-    } else {
-      pageIdx = pageIndex;
-    }
-    if (pageSize == null) {
-      log.info("no page size passed, set default value 20");
-      pageSze = Constants.DEFAULT_PAGE_SIZE;
-    } else {
-      pageSze = pageSize;
-    }
-    getImages(model, pageIdx, pageSze, false);
-    return IMAGE_MODULE_HOME + "/tr";
-  }
+	// this not valid image meta data
+	@RequestMapping(value = "/getPagerAllImageList", method = RequestMethod.POST)
+	public String getPagerAllImages(Model model, Integer pageIndex,
+			Integer pageSize) {
+		int pageIdx = -1;
+		int pageSze = 0;
+		if (pageIndex == null || pageIndex == 0) {
+			log.info("no pageindex passed, set default value 1");
+			pageIdx = Constants.DEFAULT_PAGE_INDEX;
+		} else {
+			pageIdx = pageIndex;
+		}
+		if (pageSize == null) {
+			log.info("no page size passed, set default value 20");
+			pageSze = Constants.DEFAULT_PAGE_SIZE;
+		} else {
+			pageSze = pageSize;
+		}
+		getImages(model, pageIdx, pageSze, false);
+		return IMAGE_MODULE_HOME + "/tr";
+	}
 
-  private List<ImageModel> getImages(Model model, int pageIdx, int pageSze, boolean needCheck) {
-    List<ImageModel> imgList = new ArrayList<ImageModel>();
-    try {
-      Image[] images = imageService.listImages();
-      if (images != null) {
-        ImageModel imgModel = null;
-        for (Image img : images) {
-          imgModel = new ImageModel();
-          imgModel.setImgId(img.getId());
-          imgModel.setImgName(img.getName());
-          imgModel.setCreated(img.getCreated());
-          imgModel.setMinDisk(img.getMinDisk());
-          imgModel.setMinRam(img.getMinRam());
-          imgModel.setProgress(img.getProgress());
-          imgModel.setStatus(img.getStatus());
-          imgModel.setTenant(img.getTenant());
-          imgModel.setUpdated(img.getUpdated());
-          imgModel.setUser(img.getUser());
-          if (needCheck) {
-            if (!ValidateUtil.checkValidImg(img)) {
-              continue;
-            }
-          }
-          imgList.add(imgModel);
-        }
+	private List<ImageModel> getImages(Model model, int pageIdx, int pageSze,
+			boolean needCheck) {
+		List<ImageModel> imgList = new ArrayList<ImageModel>();
+		try {
+			Image[] images = imageService.listImages();
+			if (images != null) {
+				ImageModel imgModel = null;
+				for (Image img : images) {
+					imgModel = new ImageModel();
+					imgModel.setImgId(img.getId());
+					imgModel.setImgName(img.getName());
+					imgModel.setCreated(img.getCreated());
+					imgModel.setMinDisk(img.getMinDisk());
+					imgModel.setMinRam(img.getMinRam());
+					imgModel.setProgress(img.getProgress());
+					imgModel.setStatus(img.getStatus());
+					imgModel.setTenant(img.getTenant());
+					imgModel.setUpdated(img.getUpdated());
+					imgModel.setUser(img.getUser());
+					if (needCheck) {
+						if (!ValidateUtil.checkValidImg(img)) {
+							continue;
+						}
+					}
+					imgList.add(imgModel);
+				}
 
-        PagerModel<ImageModel> page = new PagerModel<ImageModel>(imgList, pageSze);
-        imgList = page.getPagedData(pageIdx);
-        model.addAttribute("pageIndex", pageIdx);
-        model.addAttribute("pageSize", pageSze);
-        model.addAttribute("pageTotal", page.getTotalRecord());
-        model.addAttribute("dataList", imgList);
-      }
-    } catch (OpenstackAPIException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return imgList;
-  }
+				PagerModel<ImageModel> page = new PagerModel<ImageModel>(
+						imgList, pageSze);
+				imgList = page.getPagedData(pageIdx);
+				model.addAttribute("pageIndex", pageIdx);
+				model.addAttribute("pageSize", pageSze);
+				model.addAttribute("pageTotal", page.getTotalRecord());
+				model.addAttribute("dataList", imgList);
+			}
+		} catch (OpenstackAPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return imgList;
+	}
 
-  // this is for instance selection
-  @RequestMapping(value = "/imgList", method = RequestMethod.POST, produces = "application/json")
-  public @ResponseBody
-  List<ImageModel> listImages(Model model) {
-    List<ImageModel> imgModels = new ArrayList<ImageModel>();
-    try {
-      Image[] imgs = imageService.listImages();
-      ImageModel imgModel = null;
-      for (Image img : imgs) {
-        if (ValidateUtil.checkValidImg(img)) {
-          imgModel = new ImageModel();
-          imgModel.setImgId(img.getId());
-          imgModel.setImgName(img.getName());
-          imgModels.add(imgModel);
-        }
+	// this is for instance selection
+	@RequestMapping(value = "/imgList", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody
+	List<ImageModel> listImages(Model model) {
+		List<ImageModel> imgModels = new ArrayList<ImageModel>();
+		try {
+			Image[] imgs = imageService.listImages();
+			ImageModel imgModel = null;
+			for (Image img : imgs) {
+				if (ValidateUtil.checkValidImg(img)) {
+					imgModel = new ImageModel();
+					imgModel.setImgId(img.getId());
+					imgModel.setImgName(img.getName());
+					imgModels.add(imgModel);
+				}
 
-      }
-    } catch (OpenstackAPIException e) {
-      e.printStackTrace();
-    }
+			}
+		} catch (OpenstackAPIException e) {
+			e.printStackTrace();
+		}
 
-    return imgModels;
-  }
+		return imgModels;
+	}
 
-  @RequestMapping(value = "/createImage", method = RequestMethod.POST, produces = "application/json")
-  public @ResponseBody
-  Map<String, Object> createImage(Model model, ImageModel imgModel) {
+	@RequestMapping(value = "/createImage", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody
+	Map<String, Object> createImage(Model model, ImageModel imgModel) {
 
-    Map<String, Object> ret = new HashMap<String, Object>();
-    String errorMsg = ValidateUtil.validModel(validator, "admin", imgModel);
-    if (errorMsg != null) {
-      ret.put(Constants.JSON_ERROR_STATUS, errorMsg);
-      return ret;
-    }
-    try {
-      Access access = keystoneService.getAdminAccess();
-      // to do
-      ret.put(Constants.JSON_SUCCESS_STATUS, "success");
-    } catch (OpenstackAPIException e) {
-      ret.put(Constants.JSON_ERROR_STATUS, e.getMessage());
-      return ret;
-    }
+		Map<String, Object> ret = new HashMap<String, Object>();
+		String errorMsg = ValidateUtil.validModel(validator, "admin", imgModel);
+		if (errorMsg != null) {
+			ret.put(Constants.JSON_ERROR_STATUS, errorMsg);
+			return ret;
+		}
+		try {
+			Access access = keystoneService.getAdminAccess();
+			// to do
+			ret.put(Constants.JSON_SUCCESS_STATUS, "success");
+		} catch (OpenstackAPIException e) {
+			ret.put(Constants.JSON_ERROR_STATUS, e.getMessage());
+			return ret;
+		}
 
-    return ret;
-  }
+		return ret;
+	}
 
-  @RequestMapping(value = "/retrieveImage", method = RequestMethod.POST, produces = "application/json")
-  public @ResponseBody
-  String getVM(Model model, String imgId) {
+	@RequestMapping(value = "/retrieveImage", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody
+	Image getVM(Model model, String imgId) {
 
-    if (StringUtil.isNullOrEmpty(imgId)) {
-      return Constants.JSON_STATUS_FAILED;
-    }
-    try {
-      Image image = imageService.getImage(imgId);
-    } catch (OpenstackAPIException e) {
-      return Constants.JSON_STATUS_EXCEPTION;
-    }
-    // to do
-    return Constants.JSON_STATUS_DONE;
-  }
+		if (StringUtil.isNullOrEmpty(imgId)) {
+			return null;
+		}
+		Image image = null;
+		try {
+			image = imageService.getImage(imgId);
+		} catch (OpenstackAPIException e) {
+			return null;
+		}
+		// to do
+		return image;
+	}
 }

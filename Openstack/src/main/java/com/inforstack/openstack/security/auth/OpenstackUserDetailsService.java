299af99d90e169f1c1e599d60ba7f4e7322a72ab
rqshao <rqshao@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-02-21 08:44:41 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@960 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/java/com/inforstack/openstack/security/auth/OpenstackUserDetailsService.java
+++ Openstack/src/main/java/com/inforstack/openstack/security/auth/OpenstackUserDetailsService.java
@@ -1,42 +1,46 @@
 package com.inforstack.openstack.security.auth;
 
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.inforstack.openstack.log.Logger;
+import com.inforstack.openstack.tenant.Tenant;
 import com.inforstack.openstack.user.User;
 import com.inforstack.openstack.user.UserService;
 import com.inforstack.openstack.utils.OpenstackUtil;
 
-
 @Service("userDetailsService")
 public class OpenstackUserDetailsService implements UserDetailsService {
-	
-	private static final Logger log = new Logger(OpenstackUserDetailsService.class);
+
+	private static final Logger log = new Logger(
+			OpenstackUserDetailsService.class);
 	@Autowired
 	private UserService userService;
-	
+
 	private OpenstackUserDetails userDetails;
-	
+
 	@Override
+	@Transactional
 	public UserDetails loadUserByUsername(String userName)
 			throws UsernameNotFoundException {
 		log.debug("Invoke Method - loadUserByUsername");
 		User user = userService.findByName(userName);
-		if(user==null){
+		if (user == null) {
 			log.error("No user found");
-			throw new UsernameNotFoundException("No user found for user : " + userName );  
+			throw new UsernameNotFoundException("No user found for user : "
+					+ userName);
 		}
-		log.debug("User found by name : "+userName);
-		userDetails = (OpenstackUserDetails)OpenstackUtil.getBean("openstackUserDetails");
-		userDetails.setUser(user);  
-		userDetails.setTenant(user.getDefaultTenant());
+		log.debug("User found by name : " + userName);
+		userDetails = (OpenstackUserDetails) OpenstackUtil
+				.getBean("openstackUserDetails");
+		userDetails.setUser(user);
+		Tenant tenant = user.getDefaultTenant();
+		tenant.getId();
+		userDetails.setTenant(tenant);
 		return userDetails;
 	}
 

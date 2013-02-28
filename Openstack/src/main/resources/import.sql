8306c6160873a1460feac962fd170982be8f724a
lwan <lwan@445347b9-a622-0248-9c6a-1ca700c04f83>
2013-02-28 07:13:16 +0000

    git-svn-id: http://192.168.1.135:81/svn/inforsense/trunk@1064 445347b9-a622-0248-9c6a-1ca700c04f83

--- Openstack/src/main/resources/import.sql
+++ Openstack/src/main/resources/import.sql
@@ -160,6 +160,8 @@ insert into Resource(id, url, permission) values (306, '/user/instance/showInsta
 
 insert into Resource(id, url, permission) values (350, '/user/cinder/modules/index', 'user');
 insert into Resource(id, url, permission) values (351, '/user/cinder/getPagerVolumeTypeList', 'user');
+insert into Resource(id, url, permission) values (352, '/user/cinder/getPagerVolumeList', 'user');
+
 
 
 insert into Resource(id, url, permission) values (380, '/user/plan/planList', 'user');
